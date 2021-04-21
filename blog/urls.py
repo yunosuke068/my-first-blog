2ed86b6167458477bdf6b2ee52497022a3c83278
Yunosuke <yunosuke068209@gmail.com>
2021-04-21 15:57:07 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views # blogアプリの全てのビューをインポート
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # post_listをrootURLに割り当てる
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
